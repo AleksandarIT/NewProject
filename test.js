65789ab6c212010073e2edca67920e183bf7e8eb
Alex <alexandar2612@gmail.com>
2022-12-08 15:47:19 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-// test
+
+
+class Person {
+
+	constructor(name, email, phone) {
+	this.name = name;
+	this.email = email;
+	this.phone = phone;
+}
+	displayEmail() {
+		console.log(this.email);
+}
+}
+let person = new Person("aleksandar","alexandar2612@gmail.com", "065664571")
+
+console.log(person.phone); 
+
